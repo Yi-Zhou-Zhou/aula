afde6ad16ea635f3187be79a0178b76bafcc71e0
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-06-21 20:26:47 -0400

    uva2 + uva3 fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home';
 import PySano from './pages/PySano';
 import UVA1 from './pages/UVAS-PYSANO/UVA1';
+import UVA2 from './pages/UVAS-PYSANO/UVA2';
 import UVA3 from './pages/UVAS-PYSANO/UVA3';
 import UVA1_AULA from './pages/UVAS-AULA/UVA1';
 import UVA2_AULA from "./pages/UVAS-AULA/UVA2";
@@ -25,6 +26,7 @@ function App() {
           <Route path="/UVA3" element={<UVA3_AULA/>}/>
           <Route path='/PySano' element={<PySano/>}/>
           <Route path='/PySano/UVA1' element={<UVA1/>}/>
+          <Route path='/PySano/UVA2' element={<UVA2/>}/>
           <Route path='/PySano/UVA3' element={<UVA3/>}/>
         </Routes>
       </Router>
